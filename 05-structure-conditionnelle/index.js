9c1bba284609f28bef560f5ae4dfe8c81cf3f7b4
Denis Fortict <d.fortict@vivit.fr>
2021-04-26 15:43:48 +0200

    structure conditionnelle maj

--- 05-structure-conditionnelle/index.js
+++ 05-structure-conditionnelle/index.js
@@ -14,6 +14,12 @@ class App extends React.Component {
         /***
          * React a besoin d'identifier de maniere uniques nos differentes balises   * <li></li> pour mieux s'adapter lorsque les elements de la liste changent
          */
+        // for (const item of prenoms.entries()) {
+        //     const index = item[0];
+        //     const prenom = item[1];
+        //     const jsxLi = (<li key={index}>{prenom}</li>);
+        //     userJSXElements.push(jsxLi);
+        // }
         for (const [index, prenom] of prenoms.entries()) {
             const jsxLi = (<li key={index}>{prenom}</li>);
             userJSXElements.push(jsxLi);
@@ -36,6 +42,18 @@ class App extends React.Component {
                         })
                     }
                 </ul>
+                <h3>Syntaxe raccourcie</h3>
+                <ul>
+                    {
+                        prenoms.map(
+                            (prenom, index) => <li key={index}>{prenom}</li>
+                        )
+                    }
+                </ul>
+                <h2>Afficher une liste seulement si elle n'est pas vide</h2>
+                {
+
+                }
             </React.Fragment>
         );
     }
