f6067ec6dbeab4967cc984ed644892d28bc2b88f
Denis Fortict <d.fortict@vivit.fr>
2021-04-28 10:04:35 +0200

    configuration mini de react + ajout d'un tableau aliments

---
+++ 04-exercices/04-etat-d-un-composant/index.js
@@ -0,0 +1,22 @@
+const aliments = [
+    "Riz",
+    "Tomate",
+    "Poivron",
+    "Oignons"
+];
+
+
+class App extends React.Component {
+    render() {
+        return (
+        <React.Fragment>
+
+        </React.Fragment>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
