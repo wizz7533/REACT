b9cc41870fb90a5727a832b9ff47da6546014762
Denis Fortict <d.fortict@vivit.fr>
2021-04-29 10:04:56 +0200

    cycle de vie du composant: construit, rendu, monté, rendu'

--- 09-create-react-app/src/components/01-cycle_de_vie/lifeCycle.js
+++ 09-create-react-app/src/components/01-cycle_de_vie/lifeCycle.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+function FormatDate(props) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    return props.date.toLocaleDateString('fr-FR', options);
+}
 
 class LifeCycle extends Component {
     constructor(props) {
+        console.log("1.Composant Construit");
         super(props);
         this.state = {
             date: new Date()
@@ -9,12 +15,24 @@ class LifeCycle extends Component {
         }
     }
 
+    
+    componentDidMount() {
+        console.log("3.Composant Monté");
+        setInterval(() => {
+            const NewDate = new Date();
+            this.setState({ date: NewDate })
+        }, 1000);        
+    }
+
     render() {
+        console.log("2.Composant Rendu");
         return (
             <>
                 <p>
-                    Nous sommes le {this.state.date.toLocaleDateString()}
+                    Nous sommes le &nbsp;
+                    <FormatDate date={this.state.date} />
                 </p>
+                <p>Temps de connection : {this.state.date.toLocaleTimeString()}</p>
             </>
         );
     }
