c200dde2d20a88af7b16475791aed72598e4cc52
Denis Fortict <d.fortict@vivit.fr>
2021-04-29 09:22:57 +0200

    strusture initiale

---
+++ 09-create-react-app/src/components/title/Test.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+
+function Title(props) {
+    return (
+        <React.Fragment>
+            <h1>Hello World !</h1>
+        </React.Fragment>
+    );
+}
+export default Title;
